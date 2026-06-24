356892e876f504245dd9bde9de212c10500a6c15
Chanikya Raju <chanikya.raju02@gmail.com>
2026-06-24 16:41:58 +0000

    Initial commit

---
+++ variables.js
@@ -0,0 +1,53 @@
+//Variables & Datatypes
+
+// let num=10;
+// let string="chanikya";
+// let boolean=false;
+// let bigint=123456789012345678;
+// let k;
+// let nul=null;
+// console.log(typeof(num));
+// console.log(typeof(string));
+// console.log(typeof(boolean));
+// console.log(typeof(bigint));
+// console.log(typeof(k));
+// console.log(typeof(nul));
+
+
+//Loops
+
+// let arr=[1,2,3,4,5];
+// for (let n of arr){
+//     console.log(n);
+// }
+// let num=1;
+// do {
+//     console.log("printing")
+//     num++;
+// }while(num<1);
+// let people={
+//     name:"chanikya",
+//     Age: 25, 
+//     mobile:1234
+// };
+// for (let key in people){
+//     console.log(key+" "+people[key]);
+// }
+
+
+// Functions 
+function add(a,b){
+    return a+b;
+}
+add();
+console.log(add(2,3));
+
+// expression
+const sum= function(a,b){
+    return a+b;
+}
+console.log(sum(3,9));
+
+// Arrow functions
+const addition=(a,b) => a+b;
+console.log(addition(123,1234));
